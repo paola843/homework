cfe4ae0e9bcabd26798689f508bcdde8be56f46b
Paola Pumacayo <paola.pumacayo@ucsp.edu.pe>
2021-03-30 21:33:07 -0500

    Operaciones

--- 1_practica/helloworld.cpp
+++
@@ -1,5 +0,0 @@
-#include <iostream>
-int main(){
-    std::cout <<"hola"<<std::endl;
-    return 0;
-}
---
+++ 1_practica/operaciones.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+    int a, b;
+
+    std::cout<<"Ingrese 2 valores para aplicar las 4 operaciones básicas"<<std::endl;
+    std::cin>>a;
+    std::cin>>b;
+
+    std::cout<<"la suma de los valores es "<<a+b<<std::endl;
+
+    std::cout<<"la resta de los valores es "<<a- b<<std::endl;
+
+    std::cout<<"la multiplicación de los valores es "<<a*b<<std::endl;
+
+    std::cout<<"la división de los valores es "<<a/b<<std::endl;
+
+
+    return 0;
+}
