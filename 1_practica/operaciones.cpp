15514f5bd771b2a613d3a4e7f1ef21a4e0350b2b
Paola Pumacayo <paola.pumacayo@ucsp.edu.pe>
2021-03-30 21:42:40 -0500

    Operaciones Matematicas

--- 1_practica/operaciones.cpp
+++ 1_practica/operaciones.cpp
@@ -3,17 +3,16 @@
 int main(){
     int a, b;
 
-    std::cout<<"Ingrese 2 valores para aplicar las 4 operaciones básicas"<<std::endl;
-    std::cin>>a;
-    std::cin>>b;
+    a = 10;
+    b = 2;
 
-    std::cout<<"la suma de los valores es "<<a+b<<std::endl;
+    std::cout<<"la suma es: "<<a+b<<std::endl;
 
-    std::cout<<"la resta de los valores es "<<a- b<<std::endl;
+    std::cout<<"la resta es: "<<a-b<<std::endl;
 
-    std::cout<<"la multiplicación de los valores es "<<a*b<<std::endl;
+    std::cout<<"la multiplicación es: "<<a*b<<std::endl;
 
-    std::cout<<"la división de los valores es "<<a/b<<std::endl;
+    std::cout<<"la división es: "<<a/b<<std::endl;
 
 
     return 0;
